38a6bba72a3dbd58d79d0a8117627cc912b7c721
bsipiak <bartosz.sipiak@olx.pl>
2019-11-07 22:51:46 +0100

    initial commit

---
+++ src/test/kotlin/com/rest/service/restgithubapp/RestgithubappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rest.service.restgithubapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestgithubappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
